42153885c1dd07a4772bc7906ec7e0755fa10b47
Jun Lee <jun@piktochart.com>
2022-06-20 01:46:09 +0800

    feat: debounce input event and some minor changes

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -1,20 +1,21 @@
 <script lang="ts">
+import { debounce } from 'lodash-es'
 export default {
   setup (_, { emit }) {
     return {
-      onInput: (e: KeyboardEvent) => {
+      onInput: debounce((e: KeyboardEvent) => {
         // emit out custom event with search keyword
         emit('search', (e.target as HTMLInputElement).value)
-      }
+      }, 300)
     }
   }
 }
 </script>
 <template>
-  <div class="search-wrapper flex items-center rounded-full px-5 py-2">
+  <div class="search-wrapper flex items-center rounded-full px-5 py-2 w-48">
     <input
       class="text-xs w-48 italic"
-      placeholder="Type to Search..."
+      placeholder="Search movie"
       @input="onInput"/>
     <svg class="icon w-4 h-4">
       <use xlink:href="#search-outline"></use>
@@ -23,7 +24,7 @@ export default {
 </template>
 <style lang="scss" scoped>
 .search-wrapper {
-  background-color: #FFFFFF;
+  background-color: #e5e5e5;
 
   & input {
     background-color: transparent;
